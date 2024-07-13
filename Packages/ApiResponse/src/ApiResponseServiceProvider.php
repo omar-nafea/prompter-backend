5402050862f0d6bdceb0432d276e21ad5166d669
Mohamed Gaber <mgaber@tendegrees.sa>
2024-07-13 18:22:54 +0300

    enhance api response package

---
+++ Packages/ApiResponse/src/ApiResponseServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MohamedGaber\ApiResponse;
+
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Response;
+use Illuminate\Support\ServiceProvider;
+use MohamedGaber\ApiResponse\Factory\ApiResponseFactory;
+
+final class ApiResponseServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        app(ResponseFactory::class)->macro(
+            'api',
+            fn () => app(ApiResponseFactory::class)
+        );
+    }
+}
--- Packages/ApiResponse/src/helper.php
+++ Packages/ApiResponse/src/helper.php
@@ -2,11 +2,26 @@
 
 declare(strict_types=1);
 
+use MohamedGaber\ApiResponse\Builder\ErrorApiResponseBuilder;
+use MohamedGaber\ApiResponse\Builder\SuccessApiResponseBuilder;
 use MohamedGaber\ApiResponse\Factory\ApiResponseFactory;
 
 if ( ! function_exists('apiResponse')) {
     function apiResponse(): ApiResponseFactory
     {
         return app(ApiResponseFactory::class);
+    }
+}
+
+if ( ! function_exists('apiSuccess')) {
+    function apiSuccess(): SuccessApiResponseBuilder
+    {
+        return apiResponse()->success();
+    }
+}
+if ( ! function_exists('apiError')) {
+    function apiError(): ErrorApiResponseBuilder
+    {
+        return apiResponse()->error();
     }
 }
